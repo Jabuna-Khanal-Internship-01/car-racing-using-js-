c1f0a9164630e16b584e4d56cff7d609f53ac946
jabuna07 <jabuna007@gmail.com>
2021-06-15 12:49:47 +0545

    Added the trackfor racing

--- main.js
+++ main.js
@@ -1,25 +1,60 @@
 const score = document.querySelector('.score');
-const  startPlaying =document.querySelector('.startPlaying');
-const gameArea = document.querySelector('.gameArea')
+const startPlaying = document.querySelector('.startPlaying');
+const gamingArea = document.querySelector('.gamingArea')
 
 
-startPlaying.addEventListener('click',startGame); 
-let keys = { ArrowUp : false, ArrowDown : false, ArrowLeft : false, ArrowRight : false}
+startPlaying.addEventListener('click', startGame);
 
-document.addEventListener('keydown',keyPressed);
-document.addEventListener('keyup',keyReleased);
+let player = { speed: 5 };
 
-function keyPressed(e){
+
+let keys = { ArrowUp: false, ArrowDown: false, ArrowLeft: false, ArrowRight: false }
+
+document.addEventListener('keydown', keyPressed);
+document.addEventListener('keyup', keyReleased);
+
+function keyPressed(e) {
   e.preventDefault();
   keys[e.key] = true;
   console.log(keys);
 }
 
-function keyReleased(e){
+function keyReleased(e) {
   e.preventDefault();
   keys[e.key] = false;
   console.log(keys);
 }
 
+function playGame() {
+  console.log('hited');
+  let car = document.querySelector('.car');
+  let road = gamingArea.getBoundingClientRect();
+  console.log(road);
+  if (player.startGame) {
+    if (keys.ArrowUp && player.y > road.top + 20) { player.y = player.y - player.speed }
+    if (keys.ArrowDown && player.y <road.bottom-90) { player.y = player.y + player.speed }
+    if (keys.ArrowRight && player.x <450) { player.x = player.x + player.speed }
+    if (keys.ArrowLeft && player.x >0) { player.x = player.x - player.speed }
+
+
+    car.style.top = player.y + "px";
+    car.style.left = player.x + "px";
+
+    window.requestAnimationFrame(playGame);
+  }
+}
+
 function startGame() {
+  gamingArea.classList.remove('hide');
+  startPlaying.classList.add('hide');
+  player.startGame = true;
+  window.requestAnimationFrame(playGame);
+
+  let car = document.createElement('div');
+  car.setAttribute('class', 'car');
+  gamingArea.appendChild(car);
+
+  player.y = car.offsetTop;
+  player.x = car.offsetLeft;
+
 }
